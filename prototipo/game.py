648d40b79439d5085ba0adb5ca92894ecb69d1f9
marianadick <dickmariana1@gmail.com>
2022-03-11 00:33:12 -0300

    mudancas no arquivo game

--- prototipo/game.py
+++ prototipo/game.py
@@ -9,111 +9,116 @@ from Grenade import grenade_group, explosion_group
 screen_height = 600
 screen_width = 800
 
-pygame.init()
+class Game():
+    def __init__(self) -> None:
+        pass
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Metal Slug')
+    def iniciar_partida():
+        pygame.init()
 
-#set FPSd
-clock = pygame.time.Clock()
-FPS = 60
+        screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Metal Slug')
 
-#game variables
-gravity = 0.75
+        #set FPSd
+        clock = pygame.time.Clock()
+        FPS = 60
 
-#define colors
-BG = (144, 201, 120)
-RED = (255, 0, 0)
-BROWN = (110, 38, 14)
-'''
-def draw_map(current_map):
-    screen.fill(BG)
-    
-    for y in range(len(current_map)):
-        for x in range(len(current_map[y])):
-            if current_map[y][x] == 'X':
-                rect = pygame.Rect(x*50, y*50, 50, 50)
-                pygame.draw.rect(screen, BROWN, rect)
-'''   
-game_map = Map(0, 'background_1', screen_height, screen_width, 16, 150)
-#player = PlayableCharacter(200, 200, 5, 20, 5)
-#enemy = Enemy(400, 200, 5, 20)
-game_map.load_data()
-player, enemy_group, pickable_items_group = game_map.process_data()
+        #game variables
+        gravity = 0.75
 
-def health_bar(self):
-    self.health_bar = player.health
-    pygame.draw.rect(screen, (255,0,0), (10, 10, 300//2, 25))
-    pygame.draw.rect(screen, (0,255,0), (10, 10, (self.health//2)*3, 25))
+        #define colors
+        BG = (144, 201, 120)
+        RED = (255, 0, 0)
+        BROWN = (110, 38, 14)
+        '''
+        def draw_map(current_map):
+            screen.fill(BG)
+            
+            for y in range(len(current_map)):
+                for x in range(len(current_map[y])):
+                    if current_map[y][x] == 'X':
+                        rect = pygame.Rect(x*50, y*50, 50, 50)
+                        pygame.draw.rect(screen, BROWN, rect)
+        '''   
+        game_map = Map(0, 'background_1', screen_height, screen_width, 16, 150)
+        #player = PlayableCharacter(200, 200, 5, 20, 5)
+        #enemy = Enemy(400, 200, 5, 20)
+        game_map.load_data()
+        player, enemy_group, pickable_items_group = game_map.process_data()
 
-run = True
-while run:
+        def health_bar(self):
+            self.health_bar = player.health
+            pygame.draw.rect(screen, (255,0,0), (10, 10, 300//2, 25))
+            pygame.draw.rect(screen, (0,255,0), (10, 10, (self.health//2)*3, 25))
 
-    clock.tick(FPS)
+        run = True
+        while run:
 
-#   draw_map(tile_map)
-    game_map.draw_bg(screen)
-#    game_map.draw(screen)
-    health_bar(player)
+            clock.tick(FPS)
 
-    player.update()
-    player.draw(screen)
+        #   draw_map(tile_map)
+            game_map.draw_bg(screen)
+        #    game_map.draw(screen)
+            health_bar(player)
 
-    for enemy in enemy_group:
-        enemy.update()
-        enemy.draw(screen)
-        enemy.ai(player, game_map)
+            player.update()
+            player.draw(screen)
 
-    #update and draw groups
-    bullet_group.update(player, bullet_group, enemy_group)
-    bullet_group.draw(screen)
-    grenade_group.update(player, grenade_group, enemy_group)
-    grenade_group.draw(screen)
-    explosion_group.update(player, explosion_group, enemy_group)
-    explosion_group.draw(screen)
-    pickable_items_group.draw(screen)
-    pickable_items_group.update(player)
+            for enemy in enemy_group:
+                enemy.update()
+                enemy.draw(screen)
+                enemy.ai(player, game_map)
 
-    #update player actions
-    if player.alive:
-        if player.shooting:
-            player.shoot(bullet_group)
-        elif player.throwing:
-            player.throw_grenade(grenade_group)
-        player.move(game_map)
+            #update and draw groups
+            bullet_group.update(player, bullet_group, enemy_group)
+            bullet_group.draw(screen)
+            grenade_group.update(player, grenade_group, enemy_group)
+            grenade_group.draw(screen)
+            explosion_group.update(player, explosion_group, enemy_group)
+            explosion_group.draw(screen)
+            pickable_items_group.draw(screen)
+            pickable_items_group.update(player)
 
-    for event in pygame.event.get():
-        #quit game
-        if event.type == pygame.QUIT:
-            run = False
+            #update player actions
+            if player.alive:
+                if player.shooting:
+                    player.shoot(bullet_group)
+                elif player.throwing:
+                    player.throw_grenade(grenade_group)
+                player.move(game_map)
 
-        #keyboard presses
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player.moving_left = True
-            if event.key == pygame.K_d:
-                player.moving_right = True
-            if event.key == pygame.K_SPACE:
-                player.shooting = True
-            if event.key == pygame.K_q:
-                player.throwing = True
-            if event.key == pygame.K_ESCAPE:
-                run = False
-            if event.key == pygame.K_w and player.alive:
-                player.jump = True
+            for event in pygame.event.get():
+                #quit game
+                if event.type == pygame.QUIT:
+                    run = False
 
-        #keyboard button released
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                player.moving_left = False
-            if event.key == pygame.K_d:
-                player.moving_right = False
-            if event.key == pygame.K_SPACE:
-                player.shooting = False
-            if event.key == pygame.K_q:
-                player.throwing = False
-                player.grenade_thrown = False
+                #keyboard presses
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        player.moving_left = True
+                    if event.key == pygame.K_d:
+                        player.moving_right = True
+                    if event.key == pygame.K_SPACE:
+                        player.shooting = True
+                    if event.key == pygame.K_q:
+                        player.throwing = True
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
+                    if event.key == pygame.K_w and player.alive:
+                        player.jump = True
 
-    pygame.display.update()
+                #keyboard button released
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        player.moving_left = False
+                    if event.key == pygame.K_d:
+                        player.moving_right = False
+                    if event.key == pygame.K_SPACE:
+                        player.shooting = False
+                    if event.key == pygame.K_q:
+                        player.throwing = False
+                        player.grenade_thrown = False
 
-pygame.quit()
+            pygame.display.update()
+
+        pygame.quit()
