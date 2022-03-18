0ff303982a12be98fa08967b9b31d074622932ef
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-03-18 15:13:49 -0300

    getters e setters da classe Bullet

--- prototipo/Bullet.py
+++ prototipo/Bullet.py
@@ -13,9 +13,38 @@ class Bullet(pygame.sprite.Sprite):
 
     def __init__(self, x, y, direction):
         pygame.sprite.Sprite.__init__(self)
-        self.speed = 10
-        self.direction = direction
-        self.enemy = False
+        self.__speed = 10
+        self.__direction = direction
+        self.__enemy = False
+    
+    @property
+    def speed(self):
+        return self.__speed
+        
+    @speed.setter
+    def speed(self, new_speed: int):
+        if isinstance(new_speed, int):
+            self.__speed = new_speed
+    
+    @property
+    def direction(self):
+        return self.__direction
+        
+    @direction.setter
+    def direction(self, new_direction: int):
+        if isinstance(new_direction, int):
+            self.__direction = new_direction
+    
+    @property
+    def enemy(self):
+        return self.__enemy
+        
+    @enemy.setter
+    def enemy(self, new_enemy: bool):
+        if isinstance(new_enemy, bool):
+            self.__enemy = new_enemy
+
+# End of getters and setters
 
     def update(self, player, bullet_group, enemy_group):
         # move bullet
