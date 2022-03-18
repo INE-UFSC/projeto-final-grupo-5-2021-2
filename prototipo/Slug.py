9cce17404dadc84d5c1fa84445be418d5266e9a9
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-03-18 01:55:43 -0300

    getters e setters Slug

--- prototipo/Slug.py
+++ prototipo/Slug.py
@@ -13,29 +13,146 @@ class Slug(PlayableCharacter, pygame.sprite.Sprite):
         super().__init__(x, y, 5, 200, 5)
         self.image = pygame.transform.scale(slug_img, (100, 75))
         self.rect = self.image.get_rect(center=(x, y))
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
+        self.__width = self.image.get_width()
+        self.__height = self.image.get_height()
 
         # player stats before entering the slug
-        self.player_health = player_health
-        self.player_ammo = player_ammo
-        self.player_grenade = player_grenade
-        self.player_speed = player_speed
-        self.lives = lives
+        self.__player_health = player_health
+        self.__player_ammo = player_ammo
+        self.__player_grenade = player_grenade
+        self.__player_speed = player_speed
+        self.__lives = lives
 
         # slug stats
-        self.alive = True
-        self.health = 100
-        self.in_slug = False
-        self.is_human = False
+        self.__alive = True
+        self.__health = 100
+        self.__in_slug = False
+        self.__is_human = False
 
         # slug action variables
-        self.sprite_index = 0
-        self.action = 2
-        self.animation_list = slug_animation_list
-        self.update_time = pygame.time.get_ticks()
+        self.__sprite_index = 0
+        self.__action = 2
+        self.__animation_list = slug_animation_list
+        self.__update_time = pygame.time.get_ticks()
         self.image = pygame.transform.scale(self.animation_list[self.action][self.sprite_index], (100, 75))
 
+    @property
+    def width(self):
+        return self.__width
+        
+    @property
+    def height(self):
+        return self.__height
+   
+    @property
+    def player_health(self):
+        return self.__player_health
+    
+    @player_health.setter
+    def player_health(self, new_player_health: int):
+        if isinstance(new_player_health, int):
+            self.__player_health = new_player_health  
+
+    @property
+    def player_ammo(self):
+        return self.__player_ammo
+        
+    @player_ammo.setter
+    def player_ammo(self, new_player_ammo: int):
+        if isinstance(new_player_ammo, int):
+            self.__player_ammo = new_player_ammo
+    
+    @property
+    def player_grenade(self):
+        return self.__player_grenade
+    
+    @player_grenade.setter
+    def player_grenade(self, new_player_grenade: int):
+        if isinstance(new_player_grenade, int):
+            self.__player_grenade = new_player_grenade 
+
+    @property
+    def player_speed(self):
+        return self.__player_speed
+    
+    @player_speed.setter
+    def player_speed(self, new_player_speed: float):
+        self.__player_speed = new_player_speed
+    
+    @property
+    def lives(self):
+        return self.__lives
+    
+    @lives.setter
+    def lives(self, new_lives: int):
+        if isinstance(new_lives, int):
+            self.__lives = new_lives
+    
+    @property
+    def alive(self):
+        return self.__alive
+        
+    @alive.setter
+    def alive(self, new_alive: bool):
+        if isinstance(new_alive, bool):
+            self.__alive = new_alive
+     
+    @property
+    def in_slug(self):
+        return self.__in_slug
+        
+    @in_slug.setter
+    def in_slug(self, new_in_slug: bool):
+        if isinstance(new_in_slug, bool):
+            self.__in_slug = new_in_slug
+
+
+    @property
+    def is_human(self):
+        return self.__is_human
+        
+    @is_human.setter
+    def is_human(self, new_is_human: bool):
+        if isinstance(new_is_human, bool):
+            self.__is_human = new_is_human
+    
+    @property
+    def sprite_index(self):
+        return self.__sprite_index
+        
+    @sprite_index.setter
+    def sprite_index(self, new_sprite_index: int):
+        if isinstance(new_sprite_index, int):
+            self.__sprite_index = new_sprite_index
+    
+    @property
+    def action(self):
+        return self.__action
+        
+    @action.setter
+    def action(self, new_action: int):
+        if isinstance(new_action, int):
+            self.__action = new_action
+    
+    @property
+    def animation_list(self):
+        return self.__animation_list
+        
+    @animation_list.setter
+    def animation_list(self, new_animation_list: int):
+        if isinstance(new_animation_list, int):
+            self.__animation_list = new_animation_list
+    
+    @property
+    def update_time(self):
+        return self.__update_time
+        
+    @update_time.setter
+    def update_time(self, new_update_time: int):
+        if isinstance(new_update_time, int):
+            self.__update_time = new_update_time
+
+# End of getters and setters
 
     def check_alive(self):
         if self.health <= 0:
